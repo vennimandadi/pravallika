092abaa0a087a0bae17fcda79d0b5012a559c0c9
Pravallika Mandadi <venni.mandadi56782gmail.com>
2021-03-14 17:07:46 +0530

    first commit

---
+++ MyBio.java
@@ -0,0 +1,18 @@
+/**
+* Name: Pravallika
+Description : MyBio
+Date: 3/13/2021
+*/
+class MyBio{
+public static void main (String args[])
+{
+ String Name= "Pravallika";
+ String Location= "Gorijavolu";
+ String Contact= "8919622346";
+ string Mail= "venni.mandadi@gmail.com";
+System.out.println(" Name: +Name");
+System.out.println(" Location: +Location ");
+System.out.println(" Contcat: +Contact ");
+System.out.println(" Mail : +Mail");
+}
+}
